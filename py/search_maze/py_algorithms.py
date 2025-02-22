6cb30d2e19f36157339e077533e3e69887e67dde
BaoAn <nguyenchungbaoan@gmail.com>
2025-02-22 08:36:22 +0700

    Maze update

--- py/search_maze/py_algorithms.py
+++ py/search_maze/py_algorithms.py
@@ -47,3 +47,65 @@ class Maze():
         if contents.count("B") != 1:
             raise Exception("maze must have excatly one goal")
         
+        contents = contents.splitlines()
+        self.height = len (contents)
+        self.width = max(len(line) for line in contents)
+
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try: 
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i,j)
+                        row.append(False)
+                    elif contents[i][j] == "":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+
+        self.solution = None
+    
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+                if col: 
+                    print("█", end="")
+                elif (i,j) == self.start:
+                    print ("A", end = "")
+                elif (i,j) == self.goal:
+                    print("B", end = "")
+                elif solution is not None and (i, j) in solution:
+                    print ("*", end = "")
+                else: 
+                    print(" ", end = "")
+            print()
+        print()
+    
+    def neighbors(self, state):
+        row, col = state 
+        candidates = [
+            ("up", (row -1, col)),
+            ("down", (row +1, col)),
+            ("left", (row , col -1)),
+            ("right", (row, col +1)),
+        ]
+
+        result = []
+        for action, (r, c) in candidates: 
+            if 0 <= r < self.height and 0 <= c <self.width and not self.walls[r][c]:
+                result.append((action, (r,c)))
+            return result
+
+
+
+                
