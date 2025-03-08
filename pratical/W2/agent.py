744df1abbe5463a101fda2e988bbc9a48b41c758
BaoAn <nguyenchungbaoan@gmail.com>
2025-03-08 20:17:29 +1100

    color env

--- pratical/W2/agent.py
+++ pratical/W2/agent.py
@@ -325,3 +325,86 @@ class XYEnvironment(Environment):
     def turn_heading(self, heading, inc):
         """Return the heading to the left (inc=+1) or right (inc=-1) of heading."""
         return turn_heading(heading, inc)
+    
+class GraphicEnvironment(XYEnvironment):
+    
+    basicColor = {"Breeze": (225, 225, 225),
+        "Pit": (0,0,0),
+        "Gold": (253, 208, 23),
+        "Glitter": (253, 208, 23),
+        "Wumpus": (43, 27, 23),
+        "Stench": (128, 128, 128),
+        "Explorer": (0, 0, 255),
+        "Wall": (44, 53, 57), 
+                  # Agent is marked by a bright green
+        "Agent":(123, 234, 123),
+                  # Dirt is dark green
+        "Dirt": (123, 150, 150),
+                  # A cell with agent & dirt is marked by red
+        "Agent&Dirt": (255, 0, 0)
+        }
+    
+    def __init__(self, width=10, height=10, boundary=True, color=basicColor, display=False):
+        """Defin all the usual XYEnv charateristics.
+        but initialise a BlockGrid for GUI too"""
+        super().__init__(width, height)
+
+        self.grid = BlockGrid(width, height, fill=(200, 200, 200))
+        if display:
+            self.grid.show()
+            self.visible = True
+        else:
+            self.visible = False
+        self.bounded = boundary
+        self.colors = color
+
+    def get_world(self):
+        """Returns all the items in the world in a format
+        imderstandable by the ipythonblocks BlockGrids"""
+        result = []
+        x_start, y_start = (0, 0)
+        x_end, y_end = self.width, self.height
+        for x in range(x_start, x_end):
+            row = []
+            for y in range(y_start, y_end):
+                row.append(self.list_things_at((x, y)))
+            result.append(row)
+            return result
+        
+    def run(self, steps=1000, delay = 1):
+        """Run the Env for given number of time steps, 
+        but update the GUI too"""
+        for step in range(steps):
+            self.update(delay)
+            if self.is_done():
+                break
+            self.step()
+        self.update(delay)
+    
+    def update(self, delay=1):
+        sleep(delay)
+        self.reveal()
+
+    def reveal(self):
+        """Display the BlockGrid for this world - the last thing to be added at a location defines the location color"""
+        self.draw_world()
+        clear_output(1)
+        self.grid.show()
+        self.visible = True
+
+    def draw_world(self):
+        self.grid[:] = (200, 200, 200)
+        world = self.get_world()
+        for x in range(0, len(world)):
+            for y in range(0, len(world[x])):
+                if len(world[x][y]):
+                    thing_names = [thing.__class__.__name__ for thing in world[x][y]]
+                    if "Agent" in thing_names and "Dirt" in thing_names:
+                        self.grid[y, x] = self.colors["Agent&Dirt"]
+                    else:                    
+                        self.grid[y, x] = self.colors[world[x][y][-1].__class__.__name__]
+
+    def conceal(self):
+        """Hide the BlockGrid for this world"""
+        self.visible = False
+        display(HTML(''))
